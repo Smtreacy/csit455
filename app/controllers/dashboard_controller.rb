27cb0c664734b81765385fadba18e8574616093d
Tyler Roesler <troesler95@gmail.com>
2017-02-13 09:07:46 -0500

    Added authenticate to dashboard and set route home

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,11 @@
+# Dashboard - The "launching point" for the app
+# provides access point for other models/controllers
 class DashboardController < ApplicationController
-
+  # Controller to render the dashboard view
   def index
-
   end
 
+  def authenticate
+
+  end
 end
